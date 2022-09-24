3c4f1ad211ae5e7e5b905d10969ee830a000e7fe
atharvaarankalle <atharvanz@gmail.com>
2022-09-24 12:13:03 +1200

    feat: Add "Add to Cart" button and cart display

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 const app = createApp({
     data() {
         return {
+            cart: 0,
             product: "Socks",
             description: "Comfortable, soft and warm socks!",
             image: "/src/assets/images/socks_green.jpg",
