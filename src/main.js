03b0398753105b9ba1450fc6c3b8b471dab529ec
atharvaarankalle <atharvanz@gmail.com>
2022-09-24 12:07:02 +1200

    feat: Added variants list using v-for
    Also assigned id using :key tag
    This helps the DOM remember each object and keep track of unique objects with unique keys as the app changes states

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ const app = createApp({
             image: "/src/assets/images/socks_green.jpg",
             inventory: 100,
             onSale: false,
-            details: ["50% cotton", "30% wool", "20% polyester"]
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                { id: 2234, color: "green" },
+                { id: 2235, color: "blue" },
+            ]
         }
     }
 })
