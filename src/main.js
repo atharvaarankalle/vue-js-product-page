c996b9322f679cfcd4868bda5b7bafd7ce734425
atharvaarankalle <atharvanz@gmail.com>
2022-09-24 12:19:17 +1200

    feat: Add on hover functionality using @mouseover
    When the user hovers over the divs for "green" or "blue", the corresponding sock colour image is now displayed

--- src/main.js
+++ src/main.js
@@ -11,14 +11,17 @@ const app = createApp({
             onSale: false,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                { id: 2234, color: "green" },
-                { id: 2235, color: "blue" },
+                { id: 2234, color: "green", image: "/src/assets/images/socks_green.jpg" },
+                { id: 2235, color: "blue", image: "/src/assets/images/socks_blue.jpg" },
             ]
         }
     },
     methods: {
         addToCart() {
             this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
